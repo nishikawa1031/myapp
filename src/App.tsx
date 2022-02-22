a858b87d2ecf764991225a2c2550305cd97948b5
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-02-22 16:31:02 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import AppAlbum from './components/Album'
+import Dashboard from './components/dashboard/Dashboard';
+
+function App() {
+  return (
+    // <AppAlbum/>
+    <Dashboard/>
+  );
+}
+
+export default App;

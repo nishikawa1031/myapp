ce3e712c5b58e82e31f1caaf7e39cc8a98b25378
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-02-23 02:35:20 +0000

    fix test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
 import Dashboard from './components/dashboard/Dashboard';
 import { BrowserRouter } from "react-router-dom";
 
